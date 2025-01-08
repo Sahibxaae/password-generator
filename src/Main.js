b5c6a8722924c14416fefaf7261054e6f429e11a
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-08 13:47:06 +0530

    made changes in jsx for css and added few css

--- src/Main.js
+++ src/Main.js
@@ -1,60 +1,97 @@
-  import React,{useState} from 'react'
+import React, { useState } from "react";
 
-  function Main(){
-      const [password, setPassword] = useState("");
-      const [length, setLength] = useState(6);
-      const [includeUppercase, setIncludeUppercase] = useState(false);
-      const [includeLowercase, setIncludeLowercase] = useState(false);
-      const [includeNumbers, setIncludeNumbers] = useState(false);
-      const [includeSymbols, setIncludeSymbols] = useState(false);
+function Main() {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(6);
+  const [includeUppercase, setIncludeUppercase] = useState(false);
+  const [includeLowercase, setIncludeLowercase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
 
-      const generatePassword = () =>{
-          const upperCaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          const lowerCaseLetter ="abcdefghijklmnopqrstuvwxyz";
-          const numbers = 1234567890;
-          const symbols ="!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
-          let characters = "";
-          if(includeUppercase) characters += upperCaseLetters;
-          if(includeLowercase) characters += lowerCaseLetter;
-          if(includeNumbers) characters += numbers;
-          if(includeSymbols) characters += symbols;
-      
-      if(characters.length === 0){
-        alert("Please select atleast one character type");
-        return
-      }
-      let passwordGenerate = "";
-      for(let i=0;i<length;i++){
-        let random = Math.floor(Math.random()*characters.length);
-        passwordGenerate += characters[random];
-      }
-      setPassword(passwordGenerate);
-    
-    };
-     
-    return (
-      <div className='container'>
-          <h1>Password generator</h1>
-          <label htmlFor="textbox">Password length</label>
-          <input type="number" value={length} onChange={(e) => setLength(Number(e.target.value))} id="lengthbox" min="4"
-        max="20"/>
-          <div>
-              <input type="checkbox" name="include uppercase letters" id="uppercase" onChange={(e) => setIncludeUppercase(!includeUppercase)}/>
-              <label htmlFor="uppercase">include uppercase letters</label>
-              <input type="checkbox" name="include lowercase letters" id="lowercase" onChange={(e) => setIncludeLowercase(!includeLowercase)}/>
-              <label htmlFor="lowercase">include lowercase letters</label>
-              <input type="checkbox" name="include numbers" id="numbers" onChange={(e) => setIncludeNumbers(!includeNumbers)}/>
-              <label htmlFor="numbers">include numbers</label>
-              <input type="checkbox" name="include symbols" id="symbols" onChange={(e) => setIncludeSymbols (!includeSymbols) }/>
-              <label htmlFor="symbols">include symbols</label>
+  const generatePassword = () => {
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = 1234567890;
+    const symbols = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
+    let characters = "";
+    if (includeUppercase) characters += upperCaseLetters;
+    if (includeLowercase) characters += lowerCaseLetters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+
+    if (characters.length === 0) {
+      alert("Please select atleast one character type");
+      return;
+    }
+    let passwordGenerate = "";
+    for (let i = 0; i < length; i++) {
+      let random = Math.floor(Math.random() * characters.length);
+      passwordGenerate += characters[random];
+    }
+    setPassword(passwordGenerate);
+  };
+
+  return (
+    <div className="container">
+      <div className="display">
+        <h1>Password generator</h1>
+        <div className="pwd-length">
+          <label htmlFor="lengthbox">Password length:</label>
+          <input
+            type="number"
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+            id="lengthbox"
+            min="4"
+            max="20"
+          />
+        </div>
+        <div className="checkboxes">
+          <div className="check">
+            <input
+              type="checkbox"
+              name="include uppercase letters"
+              id="uppercase"
+              onChange={(e) => setIncludeUppercase(!includeUppercase)}
+            />
+            <label htmlFor="uppercase">include uppercase letters</label>
           </div>
-          <button onClick={generatePassword}>Generate password</button>
-          <div>
-              <label htmlFor="pwd">Generated password</label>
-              <input type="text" id='pwd' value={password}/>
+          <div className="check">
+            <input
+              type="checkbox"
+              name="include lowercase letters"
+              id="lowercase"
+              onChange={(e) => setIncludeLowercase(!includeLowercase)}
+            />
+            <label htmlFor="lowercase">include lowercase letters</label>
           </div>
+          <div className="check">
+            <input
+              type="checkbox"
+              name="include numbers"
+              id="numbers"
+              onChange={(e) => setIncludeNumbers(!includeNumbers)}
+            />
+            <label htmlFor="numbers">include numbers</label>
+          </div>
+          <div className="check">
+            <input
+              type="checkbox"
+              name="include symbols"
+              id="symbols"
+              onChange={(e) => setIncludeSymbols(!includeSymbols)}
+            />
+            <label htmlFor="symbols">include symbols</label>
+          </div>
+        </div>
+        <button onClick={generatePassword}>Generate password</button>
+        <div>
+          <label htmlFor="pwd">Generated password</label>
+          <input type="text" id="pwd" value={password} readOnly />
+        </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
 
-  export default Main
+export default Main;
